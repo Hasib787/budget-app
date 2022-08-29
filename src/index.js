de4ab6fc425102caff51b65c6de13b4595dacdc5
Md. Masud Rana <mr.hasib45@gmail.com>
2022-08-30 00:46:33 +0600

    Create function for data fetch and handle error

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import inquirer from "inquirer";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
-import fetch from "node-fetch";
-import { getWeekNumber, roundCurrency } from "./utils";
+import { fetchApi, getWeekNumber, roundCurrency } from "./utils.js";
 
 const filename = fileURLToPath(import.meta.url);
 const currentDir = path.dirname(filename);
-
 const questions = [
   {
     type: "input",
@@ -15,82 +13,82 @@ const questions = [
     message: "What's the file path?",
   },
 ];
-
 inquirer.prompt(questions).then((answers) => {
   const extension = answers.name.slice(-5);
   if (extension !== ".json") {
     console.log("File is not a valid json file");
     return;
   }
-
   const filePath = path.join(currentDir, answers.name);
-
   fs.access(filePath, fs.F_OK, async (err) => {
     if (err) {
       console.error(`${filePath} does not exist.`);
       return;
     }
 
-    const resCashIn = await fetch(
+    const dataCashIn = await fetchApi(
       "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in"
     );
-    const dataCashIn = await resCashIn.json();
 
-    const resCashOutNatural = await fetch(
+    const dataCashOutNatural = await fetchApi(
       "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural"
     );
-    const dataCashOutNatural = await resCashOutNatural.json();
 
-    const resCashOutLegal = await fetch(
+    const dataCashOutLegal = await fetchApi(
       "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical"
     );
-    const dataCashOutLegal = await resCashOutLegal.json();
 
-    const rawData = fs.readFileSync(filePath);
-    const inputData = JSON.parse(rawData);
+    if (dataCashIn && dataCashOutNatural && dataCashOutLegal) {
+      const rawData = fs.readFileSync(filePath);
+      const inputData = JSON.parse(rawData);
 
-    const userData = {};
+      const userData = {};
 
-    inputData.forEach((data) => {
-      const { amount } = data.operation;
+      inputData.forEach((data) => {
+        const { amount } = data.operation;
 
-      if (data.type === "cash_in") {
-        const limit = dataCashIn.max.amount;
-        let fee = (amount * dataCashIn.percents) / 100;
-        fee = fee > limit ? limit : fee;
-        console.log(roundCurrency(fee));
-      } else if (data.user_type === "juridical") {
-        const limit = dataCashOutLegal.min.amount;
-        let fee = (amount * dataCashOutLegal.percents) / 100;
-        fee = fee < limit ? limit : fee;
-        console.log(roundCurrency(fee));
-      } else {
-        const weekNumber = getWeekNumber(data.date);
-        if (userData[data.user_id]) {
-          if (userData[data.user_id][weekNumber]) {
-            userData[data.user_id][weekNumber] = {
-              amount: userData[data.user_id][weekNumber].amount + amount,
-            };
+        if (data.type === "cash_in") {
+          const limit = dataCashIn.max.amount;
+          let fee = (amount * dataCashIn.percents) / 100;
+          fee = fee > limit ? limit : fee;
+          console.log(roundCurrency(fee));
+        } else if (data.user_type === "juridical") {
+          const limit = dataCashOutLegal.min.amount;
+          let fee = (amount * dataCashOutLegal.percents) / 100;
+          fee = fee < limit ? limit : fee;
+          console.log(roundCurrency(fee));
+        } else {
+          const weekNumber = getWeekNumber(data.date);
+          if (userData[data.user_id]) {
+            if (userData[data.user_id][weekNumber]) {
+              userData[data.user_id][weekNumber] = {
+                amount: userData[data.user_id][weekNumber].amount + amount,
+              };
+            } else {
+              userData[data.user_id][weekNumber] = { amount };
+            }
           } else {
+            userData[data.user_id] = {};
             userData[data.user_id][weekNumber] = { amount };
           }
-        } else {
-          userData[data.user_id] = {};
-          userData[data.user_id][weekNumber] = { amount };
-        }
 
-        if (
-          userData[data.user_id][weekNumber].amount <=
-          dataCashOutNatural.week_limit.amount
-        ) {
-          console.log(roundCurrency(0));
-        } else {
-          const limit = dataCashOutNatural.week_limit.amount;
-          const pc = dataCashOutNatural.percents;
-          const fee = ((amount > limit ? amount - limit : amount) * pc) / 100;
-          console.log(roundCurrency(fee));
+          if (
+            userData[data.user_id][weekNumber].amount <=
+            dataCashOutNatural.week_limit.amount
+          ) {
+            console.log(roundCurrency(0));
+          } else {
+            const limit = dataCashOutNatural.week_limit.amount;
+            const pc = dataCashOutNatural.percents;
+            const fee = ((amount > limit ? amount - limit : amount) * pc) / 100;
+            console.log(roundCurrency(fee));
+          }
         }
-      }
-    });
+      });
+    } else {
+      console.log(
+        "Error in api fetching. Please check url or try again later."
+      );
+    }
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,30 @@
+import fetch from "node-fetch";
+
 export const roundCurrency = (amount, fix = 2) => {
-    const [l, r] = amount.toString().split(".");
-    return r && r.length > fix
-        ? Number(l) + parseFloat(`.${r.slice(0, fix)}`) + 1 / 10 ** fix
-        : amount.toFixed(2);
-  };
-  
-  export const getWeekNumber = (date) => {
-    const currentDate = new Date(date);
-    const yearStart = new Date(Date.UTC(currentDate.getUTCFullYear(), 0, 1));
-    const numberOfDays = Math.floor(
-        (currentDate - yearStart) / (24 * 60 * 60 * 1000)
-    );
-    return Math.ceil((currentDate.getDay() + 1 + numberOfDays) / 7);
-  };
+  const [l, r] = amount.toString().split(".");
+  return r && r.length > fix
+    ? Number(l) + parseFloat(`.${r.slice(0, fix)}`) + 1 / 10 ** fix
+    : amount.toFixed(2);
+};
+
+export const getWeekNumber = (date) => {
+  const currentDate = new Date(date);
+  const yearStart = new Date(Date.UTC(currentDate.getUTCFullYear(), 0, 1));
+  const numberOfDays = Math.floor(
+    (currentDate - yearStart) / (24 * 60 * 60 * 1000)
+  );
+  return Math.ceil((currentDate.getDay() + 1 + numberOfDays) / 7);
+};
+
+export const fetchApi = async (url) => {
+  try {
+    const data = await fetch(url);
+    if (data.status === 200) {
+      return data.json();
+    }
+    return null;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
